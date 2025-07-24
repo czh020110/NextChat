894d09511d3f4ed9ebe03b24edb6d8ec63d8e4a3
czh020110 <czh020110@163.com>
2025-07-24 22:10:23 +0800

    Evertything whill be better

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -250,8 +250,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="Chen"
+        subTitle="Everything will be better"
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
